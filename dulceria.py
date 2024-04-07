e59a1cea6b4ad69e4ff44b15d97046a82bbf0206
LilianaC <liliana.castanon@gmail.com>
2024-04-07 15:46:27 -0600

    Update dulceria.py

--- dulceria.py
+++ dulceria.py
@@ -6,6 +6,6 @@ df = pd.read_csv(url)
 
 listadulces = df['categoría'].unique().tolist()
 
-selecc = st.multiselect('Selecciona la lista', listadulces, ['Tamarindo'],help='Selecciona la categoría de dulces. Presiona Enviar 📤')
+selecc = st.selectbox('Selecciona la lista', listadulces, ['Tamarindo'],help='Selecciona la categoría de dulces. Presiona Enviar 📤')
 
 st.dataframe(df[df['categoría'] == selecc])
