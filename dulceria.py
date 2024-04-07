7ffb863bea66454a9d9896b79a1637201ad2a6a1
LilianaC <liliana.castanon@gmail.com>
2024-04-07 15:54:45 -0600

    Update dulceria.py

--- dulceria.py
+++ dulceria.py
@@ -9,3 +9,19 @@ listadulces = df['categoría'].unique().tolist()
 selecc = st.selectbox('Selecciona la lista', listadulces)
 
 st.dataframe(df[df['categoría'] == selecc])
+
+urlc = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=1699633182&single=true&output=csv'
+dfc = pd.read_csv(urlc)
+
+compra = {}
+nombre =  st.text_input('¿Cuál es tu nombre?')
+compra["nombre"] = nombre
+
+producto = st.text_input('¿Qué vas a comprar? ')
+compra["producto"] = producto
+
+cantidad = st.number_input('¿Cuántas piezas?',value=0)
+compra["cantidad"] = cantidad
+
+dfc.loc[0] = compra
+st.dataframe(dfc)
