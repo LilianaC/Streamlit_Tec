24a2520e5fc40c7087284b9dd6184e2c39b15faf
LilianaC <liliana.castanon@gmail.com>
2024-07-10 11:00:36 -0600

    Create EditDataFrames.py

---
+++ EditDataFrames.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=65918408&single=true&output=csv'
+df = pd.read_csv(url)
+
+st.dataframe(df, use_container_width=True)
