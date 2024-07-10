0235dbadbc30c10630a719a0b3519866cf8ed522
LilianaC <liliana.castanon@gmail.com>
2024-07-10 15:55:54 -0600

    Create EdicionDataframeEjemplo.py

---
+++ EdicionDataframeEjemplo.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=65918408&single=true&output=csv'
+df = pd.read_csv(url)
+urlc = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=1699633182&single=true&output=csv'
+dfc = pd.read_csv(urlc)
+
+st.title('🍬 La dulcería: Editable ✍🏻')
+edited_df = st.data_editor(df)
+edited_dfc = st.data_editor(dfc)
