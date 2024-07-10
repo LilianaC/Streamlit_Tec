031216c0d16ab01950048406618ffe768ce46ae0
LilianaC <liliana.castanon@gmail.com>
2024-07-10 11:05:36 -0600

    Update EditDataFrames.py

--- EditDataFrames.py
+++ EditDataFrames.py
@@ -5,4 +5,5 @@ from PIL import Image
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=65918408&single=true&output=csv'
 df = pd.read_csv(url)
 
+st.title('🎙️ Dataframe de Spotify 🎼')
 st.dataframe(df, use_container_width=True)
