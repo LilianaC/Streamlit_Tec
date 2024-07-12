24423c1411541faeda814972f6ee455165d11218
LilianaC <liliana.castanon@gmail.com>
2024-07-11 20:26:02 -0600

    Create Dulceria_Dataframe_1.py

---
+++ Dulceria_Dataframe_1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQy_3VusOhQHehdbyBITYO3YkIeZ9agx3SDvkcCk0s02Yo9jqB_2c5wS2O7x5cdo1KXavy_tlqbNvHy/pub?gid=504292800&single=true&output=csv'
+df = pd.read_csv(url)
+
+
+st.title('🍬 La dulcería: El dataframe ✍🏻')
+st.dataframe(df, use_container_width=True)
