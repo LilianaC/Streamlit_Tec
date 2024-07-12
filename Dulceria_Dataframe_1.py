f7f8133c0720bd24bb048883241d7c4cc857086c
LilianaC <liliana.castanon@gmail.com>
2024-07-11 21:15:59 -0600

    Update Dulceria_Dataframe_1.py

--- Dulceria_Dataframe_1.py
+++ Dulceria_Dataframe_1.py
@@ -8,7 +8,7 @@ df = pd.read_csv(url)
 st.title('🍬 La dulcería: El dataframe ✍🏻')
 
 st.dataframe(df, 
-             width=150, height=450, use_container_width=True,
+             width=150, height=420, use_container_width=True,
              hide_index=True,
              column_order=['producto','Fotografía','precio','categoría'],
              column_config={
