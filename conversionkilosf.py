49400a2a365e0234d8f0c55efa508d44d292bd9e
LilianaC <liliana.castanon@gmail.com>
2024-07-19 17:38:03 -0600

    Update conversionkilosf.py

--- conversionkilosf.py
+++ conversionkilosf.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="wide", page_title="Column config demo app", page_icon="🧮"
 )
 icon("🖩")
-st.title('Convertidor de kg a kg-fuerza 🖩')
+st.title('Ejemplos de Session state (Estado de las sesiones)')
 
 count = 0
 
@@ -22,24 +22,4 @@ if increment:
 st.write('Count = ', count)
 
 
-
-def kgs_kgf():
-  st.session_state.kgsf = st.session_state.kgs * 9.80665
-
-  
-def kgf_kg():
-  st.session_state.kgs = st.session_state.kgsf / 9.80665
-  
-
-"st.session_state object:",st.session_state
-
-col1, buff, col2 = st.columns([2,1,2])
-
-with col1:
-  kilos = st.number_input("kilos:",key="kgs",
-                          on_change = kgs_kgf)
-
-with col2:
-  fuerza = st.number_input("kilos fuerza:",key="kgsf",
-                           on_change = kgf_kg)
 
