066226f321d55f5c18bd708f547578ff9525cd1c
LilianaC <liliana.castanon@gmail.com>
2024-07-19 21:48:39 -0600

    Update sessionstate1.py

--- sessionstate1.py
+++ sessionstate1.py
@@ -9,9 +9,9 @@ def icon(emoji: str):
     )
 
 st.set_page_config(
-    layout="wide", page_title="Column config demo app", page_icon="🧮"
+    layout="wide", page_title="Ejemplo de uso de Session state", page_icon="🍪"
 )
-icon("🖩")
+icon("🍪")
 st.title('Ejemplos de Session state (Estado de las sesiones)')
 
 st.markdown(''':orange[El botón solamente funciona una vez]''')
@@ -48,23 +48,3 @@ def limpiar_cache():
 st.button("Borrar 🧹", on_click= limpiar_cache)
 
 st.markdown(''':blue[Ahora una prueba para enviar información fuera de un formulario]''')
-
-
-
-import datetime
-
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-    st.session_state.last_updated = datetime.time(0,0)
-
-def update_counter():
-    st.session_state.count += st.session_state.increment_value
-    st.session_state.last_updated = st.session_state.update_time
-
-with st.form(key='my_form'):
-    st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
-    st.number_input('Enter a value', value=0, step=1, key='increment_value')
-    submit = st.form_submit_button(label='Update', on_click=update_counter)
-
-st.write('Current Count = ', st.session_state.count)
-st.write('Last Updated = ', st.session_state.last_updated)
