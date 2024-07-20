d1ab9c15ae1f3f8dacea3e3bd8dd5536c7538f5d
LilianaC <liliana.castanon@gmail.com>
2024-07-19 19:24:34 -0600

    Update conversionkilosf.py

--- conversionkilosf.py
+++ conversionkilosf.py
@@ -38,14 +38,23 @@ if st.session_state.counter <= 5:
 elif st.session_state.counter > 5:
     st.success("¡Ya alcanzaste la cuenta necesaria para el 🏆 " + str(st.session_state.counter))
 
-st.markdown(''':blue[El botón funciona todas las veces]''')
-st.title('Counter Example using Callbacks')
+st.markdown(''':blue[Otra prueba]''')
+
+import datetime
+
+st.title('Counter Example')
 if 'count' not in st.session_state:
     st.session_state.count = 0
+    st.session_state.last_updated = datetime.time(0,0)
 
-def increment_counter():
-    st.session_state.count += 1
+def update_counter():
+    st.session_state.count += st.session_state.increment_value
+    st.session_state.last_updated = st.session_state.update_time
 
-st.button('Increment', on_click=increment_counter)
+with st.form(key='my_form'):
+    st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
+    st.number_input('Enter a value', value=0, step=1, key='increment_value')
+    submit = st.form_submit_button(label='Update', on_click=update_counter)
 
-st.write('Count = ', st.session_state.count)
+st.write('Current Count = ', st.session_state.count)
+st.write('Last Updated = ', st.session_state.last_updated)
