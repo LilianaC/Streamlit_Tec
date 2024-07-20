a400be8b83fa7755d0978bc4429a196d711f13d0
LilianaC <liliana.castanon@gmail.com>
2024-07-19 18:24:51 -0600

    Update conversionkilosf.py

--- conversionkilosf.py
+++ conversionkilosf.py
@@ -26,7 +26,7 @@ st.markdown(''':green[El botón funciona todas las veces]''')
 
 st.write(
         """
-        ## 💯 El contador que sí funciona
+        ## :purple[💯 El contador que sí funciona]
 
         """
     )
